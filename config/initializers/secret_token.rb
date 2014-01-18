e3767f90fd8d686ddca4769656cae2e04e6ccca8
wonjooncha <chawonjun11@gmail.com>
2014-01-18 20:51:43 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wjcha2::Application.config.secret_key_base = 'b17d9be153e5ea1738bdd010661fe0cc64cf5bdb125b2ee1f2634cd2e46ebbbc9596734e65435b2c7f4aeb5470b98c5a5a42f1b2f9bcf3f693f9b7a375f13be9'
